aecf6f158ef6cb0a0f56fbf992b08690bdefe465
SabahGuri <guri.sabah@icloud.com>
2023-09-05 19:24:57 +0200

    form di login e registrazione

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+
+  <form action="/login" method="POST">
+    @csrf
+
+    <div class="mb-3">
+      <label for="email" class="form-label">Email address</label>
+      <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+      <div id="emailHelp" class="form-text">Non condivideremo la tua e-mail con nessuno!</div>
+    </div>
+    <div class="mb-3">
+      <label for="password" class="form-label">Password</label>
+      <input type="password" name="password" class="form-control" id="password">
+    </div>
+    <button type="submit" class="btn btn-primary">Accedi</button>
+  </form>
+
+</x-layout>
