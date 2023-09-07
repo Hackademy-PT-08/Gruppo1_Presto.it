1bec1da8bc40758c57c0c2e4a673a0a4363ab522
Gambo16b <giuseppe.rollo.00@gmail.com>
2023-09-07 21:59:52 +0200

    sfondo_homepage_merge

--- resources/views/homepage/homepage.blade.php
+++ resources/views/homepage/homepage.blade.php
@@ -1,30 +1,32 @@
 <x-layout>
 
     ì
-{{-- hero --}}
-<section id="Hero">
-    <div class="content">
+    {{-- hero --}}
+    <section id="Hero">
+        <div class="content">
 
-        <div class="container">
-            <div class="row">
-                <div class="col-12 text-centre ">
-                    <h1 class=" text-center" id="home_title">Crea e trova gli annunci migliori <br> per te e la tua casa</h1>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-centre ">
+                        <h1 class=" text-center" id="home_title">Crea e trova gli annunci migliori <br> per te e la tua
+                            casa</h1>
+                    </div>
                 </div>
+                @auth
+                    <div class="text-center">
+                        <a class="btn btn-outline-danger btn-lg" aria-disabled="true"
+                            href="{{ route('announcements.create') }}">Crea
+                            annuncio</a>
+                    </div>
+                @endauth
+
             </div>
 
         </div>
-
-    </div>
-    <video autoplay muted loop id="myvideo">
-        <source src="/media/video-back.mp4" type="video/mp4">
-      </video>
-      <div class="blur"></div>
-</section>
-    @auth
-        <div class="text-center">
-            <a class="btn btn-outline-danger btn-lg" aria-disabled="true" href="{{ route('announcements.create') }}">Crea
-                annuncio</a>
-        </div>
-    @endauth
+        <video autoplay muted loop id="myvideo">
+            <source src="/media/video-back.mp4" type="video/mp4">
+        </video>
+        <div class="blur"></div>
+    </section>
 
 </x-layout>
