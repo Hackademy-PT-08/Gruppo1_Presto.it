56da3e534bb686ea9bd8048f7b39082d43388e30
Gambo16b <giuseppe.rollo.00@gmail.com>
2023-09-07 21:45:00 +0200

    sfondo_homepage

--- resources/views/homepage/homepage.blade.php
+++ resources/views/homepage/homepage.blade.php
@@ -1,11 +1,23 @@
 <x-layout>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="my-5 text-center" id="home_title">Presto.it</h1>
-            </div>
-        </div>
-    </div>
+    ì
+{{-- hero --}}
+<section id="Hero">
+    <div class="content">
 
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-centre ">
+                    <h1 class=" text-center" id="home_title">Crea e trova gli annunci migliori <br> per te e la tua casa</h1>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <video autoplay muted loop id="myvideo">
+        <source src="/media/video-back.mp4" type="video/mp4">
+      </video>
+      <div class="blur"></div>
+</section>
 </x-layout>
