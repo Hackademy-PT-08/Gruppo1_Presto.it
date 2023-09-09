bd7e6b73494208bd5206d0f3c3ea9bc0c640d377
FabioRacc <fabi.lisci@gmail.com>
2023-09-09 13:37:22 +0200

    Cards annunci aggiornate

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,16 +1,30 @@
-<div class="card p-0" id="card_container" style="width: 18rem; height: fit-content">
-    <img src="{{ $image }}" id="img_card" class="card-img-top" alt="...">
-    <div class="card-body">
-        <span class="badge text-bg-primary mt-1 card-category"><a class="a-modified" href="{{$hrefCategory}}">{{ $category }}</a></span>  
-        <h5 class="card-title mt-3 text-center">{{ $title }}</h5>
-        <p class="card-description card-text mt-3">{{ $description }}</p>
-        <p class="card-price mt-3 h5">{{ $price }}€</p>
-        <div class="d-flex justify-content-between">
-            <a href="{{ $hrefSingle }}" class="btn btn-success">Vedi annuncio</a>
-
-            @if (Auth::user() && Auth::user()->id == $userId)
-                <a href="{{ $hrefModify }}" class="btn btn-primary">Modifica</a>
-            @endif
-        </div>
+  <article class="card h-100 border-0 shadow">
+    <div class="card-img-top position-relative overflow-hidden">
+      <a class="d-block" href="{{ $hrefSingle }}">
+        <img src="{{ $image }}" alt="Product image">
+      </a>
+      <!-- Edit button-->
+      @if (Auth::user() && Auth::user()->id == $userId)
+        <a class="btn-edit btn-sm position-absolute top-0 end-0 m-2" href="{{ $hrefModify }}" data-bs-toggle="tooltip"
+          data-bs-placement="left" aria-label="Edit" data-bs-original-title="Edit">
+          <i class="fa-regular fa-pen-to-square"></i>
+        </a>
+      @endif
+      <span class="badge text-bg-secondary position-absolute bottom-0 start-0 m-2">{{ $category }}</span>
     </div>
-</div>
+    <div class="card-body">
+      <h3 class="product-title mb-2 fs-6">
+        <a class="d-block text-truncate" href="{{ $hrefSingle }}">{{ $title }}</a>
+      </h3>
+      <span class="fs-sm text-muted">{{ $description }}</span>
+    </div>
+    <div class="card-footer mt-n1 py-0 border-0">
+      <div class="d-flex align-items-center justify-content-between position-relative mb-1 py-3 border-top">
+        <div class="user">
+          <img class="me-2 rounded-circle" src="https://picsum.photos/300/300" width="32" alt="Avatar">
+          <a class="nav-link-style fs-sm stretched-link" href="#">Utente</a>
+        </div>
+        <h4 class="mt-1 mb-0 fs-6 text-darker">{{ $price }}€</h4>
+      </div>
+    </div>
+  </article>
--- resources/views/livewire/index-announcement.blade.php
+++ resources/views/livewire/index-announcement.blade.php
@@ -1,58 +1,21 @@
 <div>
-    {{--   
-    <div class="container my-5">
-        <div class="row">
-            <div class="col-8"></div>
-            <div class="col-4">
-                <form class="d-flex gap-3" role="search">
-                    <div class="col-7">
-                        <input wire:model="query_name" wire:keyup.debounce="reloadPosts" class="form-control me-2"
-                            type="search" placeholder="Search" aria-label="Cerca">
-                    </div>
-                    <div class="col-5">
-                        <select wire:model="query_category_id" wire:change="reloadPosts" class="form-select"
-                            aria-label="Default select example">
-                            <option selected value="all">Categorie</option>
-                            @foreach ($categories as $category)
-                                <option value="{{ $category->id }}">{{ $category->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </form>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-1 gy-sm-4 g-4 mb-4">
 
-        <div class="container d-flex">
-            <div class="row">
-                @foreach ($announcements as $announcement)
-                    <div class="d-flex col-md-3 col-sm-12">
-                        <x-card userId="{{ $announcement->user_id }}" image="https://picsum.photos/300/300"
-                            title="{{ $announcement->title }}" description="{{ $announcement->description }}"
-                            price="{{ $announcement->price }}" category="{{ $announcement->category->name }}"
-                            hrefCategory="{{ route('searchByCategory', $announcement->category->id) }}"
-                            hrefSingle="{{ route('announcements.single', $announcement->id) }}"
-                            hrefModify="{{ route('announcements.edit', $announcement->id) }}" />
-                    </div>
-                @endforeach
-            </div>
+      @foreach ($announcements as $announcement)
+        <div class="col">
+          <x-card userId="{{ $announcement->user_id }}" image="https://picsum.photos/300/300"
+            title="{{ $announcement->title }}" description="{{ $announcement->description }}"
+            price="{{ $announcement->price }}" category="{{ $announcement->category->name }}"
+            hrefCategory="{{ route('searchByCategory', $announcement->category->id) }}"
+            hrefSingle="{{ route('announcements.single', $announcement->id) }}"
+            hrefModify="{{ route('announcements.edit', $announcement->id) }}" />
         </div>
-    </div> --}}
+      @endforeach
 
-    <div class="container py-5">
-        <div class="row justify-content-center">
-            @foreach ($announcements as $announcement)
-                <div class="col-12 col-lg-4">
-                    <div class="card box-shadow mx-auto my-5" style="width: 18rem;">
-                        <x-card userId="{{ $announcement->user_id }}" image="https://picsum.photos/300/300"
-                            title="{{ $announcement->title }}" description="{{ $announcement->description }}"
-                            price="{{ $announcement->price }}" category="{{ $announcement->category->name }}"
-                            hrefCategory="{{ route('searchByCategory', $announcement->category->id) }}"
-                            hrefSingle="{{ route('announcements.single', $announcement->id) }}"
-                            hrefModify="{{ route('announcements.edit', $announcement->id) }}" />
-                    </div>
-                </div>
-            @endforeach
-        </div>
     </div>
+  </div>
+</div>
+</div>
 
 </div>
