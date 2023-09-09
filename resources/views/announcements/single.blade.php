a30224aaf2cf7cc21f61cecfb415e7aa66be1def
FabioRacc <fabi.lisci@gmail.com>
2023-09-09 20:30:47 +0200

    Inizio del singolo annuncio - Da finire

--- resources/views/announcements/single.blade.php
+++ resources/views/announcements/single.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="container mt-5">
+    {{-- <div class="container mt-5">
         <div class="row">
             <div class="col-6">
                 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
@@ -39,5 +39,46 @@
             </div>
         </div>
     </div>
-    </div>
+    </div> --}}
+
+    <div class="container mb-5 pb-3">
+        <div class="bg-light shadow-lg rounded-3 overflow-hidden">
+          <div class="row">
+            <!-- Sidebar-->
+            <section class="content col-lg-5 pt-lg-4 pb-4 mb-3">
+
+                <!-- Sezione informazioni -->
+
+                <div id="carouselExample" class="carousel slide">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="https://picsum.photos/300/300" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="https://picsum.photos/300/300" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="https://picsum.photos/300/300" class="d-block w-100" alt="...">
+                      </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+
+              </section>
+            <!-- Content-->
+            <section class="content col-lg-7 pt-lg-4 pb-4 mb-3">
+
+
+
+            </section>
+          </div>
+        </div>
+      </div>
 </x-layout>
