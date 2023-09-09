eeb1eae75fef251eeb6ca8f63bd2d233b3e8a77c
L0R3NZ098 <lorenzo.cennamo98@gmail.com>
2023-09-09 10:18:23 +0200

    modifica login e register con testo scuro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
-    <h3 class="text-light text-center mt-5">Registrati</h3>
+    <h3 class="text-center mt-5">Registrati</h3>
 
     <div class="container">
         <div class="row justify-content-center">
@@ -9,31 +9,31 @@
                     @csrf
 
                     <div class="mb-3">
-                        <label for="name" class="form-label text-light mt-5">Nome</label>
+                        <label for="name" class="form-label mt-5">Nome</label>
                         <input type="text" name="name" class="form-control" id="name">
                         @error('name')
                             <p> {{ $message }} </p>
                         @enderror
                     </div>
                     <div class="mb-3">
-                        <label for="email" class="form-label text-light mt-3">Email</label>
+                        <label for="email" class="form-label mt-3">Email</label>
                         <input type="email" name="email" class="form-control" id="email"
                             aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text text-light">Non condivideremo la tua e-mail con nessuno!
+                        <div id="emailHelp" class="form-text">Non condivideremo la tua e-mail con nessuno!
                         </div>
                         @error('email')
                             <p> {{ $message }} </p>
                         @enderror
                     </div>
                     <div class="mb-3">
-                        <label for="password" class="form-label text-light mt-3">Password</label>
+                        <label for="password" class="form-label mt-3">Password</label>
                         <input type="password" name="password" class="form-control" id="password">
                         @error('password')
                             <p> {{ $message }} </p>
                         @enderror
                     </div>
                     <div class="mb-3">
-                        <label for="password_confirmation" class="form-label text-light mt-3">Conferma Password</label>
+                        <label for="password_confirmation" class="form-label mt-3">Conferma Password</label>
                         <input type="password" name="password_confirmation" class="form-control"
                             id="password_confirmation">
                         @error('password_confirmation')
