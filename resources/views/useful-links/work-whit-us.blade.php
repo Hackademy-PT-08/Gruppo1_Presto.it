a4ce8761a87b34d085da76bd3fb66c3c563b12a7
L0R3NZ098 <lorenzo.cennamo98@gmail.com>
2023-09-12 21:00:44 +0200

    aggiunta form di candidatura

--- resources/views/useful-links/work-whit-us.blade.php
+++ resources/views/useful-links/work-whit-us.blade.php
@@ -15,6 +15,8 @@
         </div>
     </div>
 
+    {{-- Section description --}}
+
     <section class="container pb-md-4">
         <div class="bg-light shadow-lg rounded-3 px-4 py-lg-4 py-3 mb-5">
             <div class="py-lg-3 py-2 px-lg-3">
@@ -91,56 +93,25 @@
 
     </section>
 
-    <section>
+    {{-- Section login revisor --}}
+
+    <section class="my-5">
         <div class="login container py-2">
             <div class="row justify-content-center">
                 <div class="col-11 col-sm-10 col-md-8  shadow-lg rounded">
                     <div class="row">
-                        <div class="col px-0 d-none d-lg-block">
-                            <img src="/media/login-image.jpg">
-                        </div>
+
                         <div class="col py-4 px-4">
-                            <h1 class="h2 text-center"> Login </h1>
+                            <h1 class="h2 text-center"> Candidati </h1>
                             <form class="form" action="/login" method="post">
                                 @csrf
 
-                                <div class="row gy-3">
-                                    <div class="col-12">
-                                        <label class="form-label" for="name">Email</label>
-                                        <input class="form-control" name="email" id="email" type="email"
-                                            placeholder="Inserisci la tua email">
-                                        @error('email')
-                                            <span class="small text-danger"> {{ $message }} </span>
-                                        @enderror
-                                    </div>
-                                    <div class="col-12">
-                                        <label class="form-label" for="password">Password</label>
-                                        <input class="form-control" name="password" id="password" type="password"
-                                            placeholder="Inserisci la tua password">
-                                        @error('email')
-                                            <span class="small text-danger"> {{ $message }} </span>
-                                        @enderror
-                                    </div>
+                                <div class="row">
+                                    <textarea name="Candidati" class="form-control" placeholder="Perchè vuoi lavorare con noi?" id="Candidati"
+                                        cols="30" rows="10"></textarea>
                                 </div>
-                                <div class="d-flex justify-content-between">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value=""
-                                            id="flexCheckDefault">
-                                        <label class="form-check-label small" for="flexCheckDefault">
-                                            Ricordami
-                                        </label>
-                                    </div>
-                                    <button class="password-forget form-link small mb-3">Password dimenticata? </a>
 
-                                </div>
-                                <div class="d-flex flex-sm-row flex-column">
-                                    <button class="btn btn-primary" type="submit">Accedi</button>
-                                </div>
-                                <hr>
-                                <div class="d-flex flex-sm-row flex-column">
-                                    <p class="small">Non hai ancora un account? <a class="form-link"
-                                            href="/register">Registrati</a></p>
-                                </div>
+                                <input class="mt-3 ms-0 btn btn-primary" type="submit" value="Invia la tua richiesta">
 
                             </form>
                         </div>
