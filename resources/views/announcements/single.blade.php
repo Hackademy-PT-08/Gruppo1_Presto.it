953dde21fdd92c36dcd121267f1da3262d6016a7
FabioRacc <fabi.lisci@gmail.com>
2023-09-13 18:54:21 +0200

    Lievi modifiche grafiche alla vista profile

--- resources/views/announcements/single.blade.php
+++ resources/views/announcements/single.blade.php
@@ -106,67 +106,48 @@
             <div class="position-relative rounded-3 overflow-hidden mb-lg-4 mb-3 "><img class="w-100 rounded "
                 src="https://picsum.photos/400/300"></div>
           </div>
-          {{-- description --}}
           <div class="col-lg-6">
-            <div class="ps-xl-5 ps-lg-3">
+            <div>
               {{-- titolo --}}
               <div class="text-center">
                 <h2 class="h3">{{ $announcement->title }}</h2>
               </div>
-              <div class="d-flex aling-items-centre flex-wrap text-nowrap mb-sm-4 mb-3 fs-sm ">
-                <div class="mb-2 me-sm-3 me-2">creato il:{{ $announcement->created_at->format('d/m/Y') }}</div>
-                <div class="mb-2 me-sm-3 me-2 ps-sm-3 ps-2 border-start"><i class="fas fa-eye me-2"></i>15 views</div>
-                <div class="mb-2 me-sm-3 me-2 ps-sm-3 ps-2 border-start"><i class="far fa-heart me-2"></i>4 favorite
+              <div class="d-flex aling-items-centre flex-wrap text-nowrap mb-sm-4 mb-3 small ">
+                <div class="mb-2 me-sm-3 me-2">
+                  <i class="fa-regular fa-calendar me-2"></i>
+                  {{ $announcement->created_at->format('d/m/Y') }}
                 </div>
-              </div>
-
-              <div class="row row-cols-sm-2 row-cols-1 gy-3 gx-4 mb-4 pb-md-2">
-                {{-- creators --}}
-                <div class="col">
-                  <div class="card position-relative h-100">
-                    <div class="card-body p-3">
-                      <h3 class="h6 mb-2 fs-sm text-muted">{{ $announcement->user->name }}</h3>
-                      <div class="d-flex align-items-center">
-                        <img class="rounded-circle me-2" src="https://picsum.photos/300/300" width="36">
-                        <a class="ms-2" href="">@foxnet_creator</a>
-                      </div>
-                    </div>
-                  </div>
+                <div class="mb-2 me-sm-3 me-2">
+                  <i class="fa-regular fa-user me-2"></i>
+                  {{ $announcement->user->name }}
                 </div>
-                {{-- revisor --}}
-                <div class="col">
-                  <div class="card position-relative h-100">
-                    <div class="card-body p-3">
-                      <h3 class="h6 mb-2 fs-sm text-muted">Revisor</h3>
-                      <div class="d-flex align-items-center"><img class="rounded-circle me-2"
-                          src="https://picsum.photos/300/300" width="36""><a class="ms-2 "
-                          href="nft-catalog-v2.html">Cartzilla art collage</a></div>
-                    </div>
-                  </div>
-                </div>
-                
-
-              </div>
-              <p class="mb-4 pb-md-2 fs-sm"> {{ $announcement->description }} </p>
-
-              <div class="row row-cols-sm-2 row-cols-1 gy-3 mb-4 pb-md-2">
-                <div class="col">
-                  <h3 class="h6 mb-2 fs-sm text-muted">Prezzo</h3>
-                  <h2 class="h3 mb-1"> € {{ $announcement->price }}</h2>
-                </div>
-                <div class="col">
-                  <h3 class="h6 mb-2 fs-sm text-muted">Categoria</h3>
-                  <h2 class="h3 mb-1"> {{ $announcement->category->name }}</h2>
+                <div class="mb-2 me-sm-3 me-2">
+                  <i class="fa-solid fa-car me-2"></i>
+                  {{ $announcement->category->name }}
                 </div>
               </div>
             </div>
+            {{-- description --}}
+            <p class="mb-4 pb-md-2 fs-sm"> {{ $announcement->description }} </p>
 
+            <div class="row row-cols-sm-2 row-cols-1 gy-3 mb-4 pb-md-2">
+              <div class="col">
+                <h3 class="h6 mb-2 fs-sm text-muted">Prezzo</h3>
+                <h2 class="h3 mb-1"> € {{ $announcement->price }}</h2>
+              </div>
+              <div class="col">
+                <h3 class="h6 mb-2 fs-sm text-muted">Categoria</h3>
+                <h2 class="h3 mb-1"> {{ $announcement->category->name }}</h2>
+              </div>
+            </div>
           </div>
-
 
         </div>
 
       </div>
+    </div>
+
+    </div>
 
   </section>
 
