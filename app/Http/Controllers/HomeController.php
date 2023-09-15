da174992327d68cacd06b71c06663680346d968d
FabioRacc <fabi.lisci@gmail.com>
2023-09-15 17:11:05 +0200

    Aggiunto controllo revisione annunci random

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
     public function homepage(){
 
         $announcements=Announcement::latest()->take(6)->get();
-        $random_announcements=Announcement::inRandomOrder()->take(3)->get();
+        $random_announcements=Announcement::inRandomOrder()->where('is_revised',true)->take(3)->get();
         $revised_announcements= $announcements->where('is_revised',true);
         return view('homepage.homepage',compact('revised_announcements', 'random_announcements'));
     }
